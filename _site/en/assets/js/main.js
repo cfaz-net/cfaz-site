dd605399862685da9dce5550516cf51983b41f14
Scheilla <scheillaartifon9805@hotmail.com>
2021-07-05 15:45:33 -0300

    Modificando código do Hotjar

--- _site/en/assets/js/main.js
+++ _site/en/assets/js/main.js
@@ -319,13 +319,13 @@
    */
   (function(h,o,t,j,a,r){
     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:2448160,hjsv:6};
+    h._hjSettings={hjid:2487918,hjsv:6};
     a=o.getElementsByTagName('head')[0];
     r=o.createElement('script');r.async=1;
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
     a.appendChild(r);
-  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-
+  })(window,document,'https://static.hotjar.com/c/hotjar-%27,%27.js?sv=%27');
+  
   /**
    * translation by browser
    */
