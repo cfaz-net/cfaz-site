8370d9672f7327fdf8a483c60b1de1cb8331e96d
Scheilla <scheillaartifon9805@hotmail.com>
2021-07-20 11:50:59 -0300

    Instalando pixell

--- en/assets/js/main.js
+++ en/assets/js/main.js
@@ -326,6 +326,17 @@
     a.appendChild(r);
   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 
+  !function(f,b,e,v,n,t,s){
+    if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js%27');
+    fbq('init', '4002307449886727');
+    fbq('track', 'PageView');
+
   /**
    * translation by browser
    */
