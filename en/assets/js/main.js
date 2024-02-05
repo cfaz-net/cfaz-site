7869049deedff510f6215f906ce723ced83182e2
Scheilla <scheillaartifon9805@hotmail.com>
2024-02-05 15:59:00 -0300

    Adicionado tamanho em imagens e arrumado tradução da imagem de fluxo de gestao na aba de vet

--- en/assets/js/main.js
+++ en/assets/js/main.js
@@ -100,6 +100,23 @@
     window.location = '/en/';
   }
 
+  // Tradução imagem Fluxo de Getsão Cfaz (vet)
+  window.addEventListener('load', () => {
+    let language = document.getElementById('language');
+    
+    if (language.value == 'es'){
+      let image_es = document.getElementById("image-es");
+      image_es.classList.remove("disappear");
+    } else if (language.value == 'en'){
+      let image_en = document.getElementById("image-en");
+      console.log("entrou en")
+      image_en.classList.remove("disappear");
+    } else {
+      let image = document.getElementById("image-pt");
+      image.classList.remove("disappear");
+    }
+  });
+
   // Analytics
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
